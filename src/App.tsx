06d2c3eb2c01aa83e6cb448c36bfdd9b99c13f5b
tylerjonesHCSS <77555767+tylerjonesHCSS@users.noreply.github.com>
2021-01-16 17:18:41 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import * as React from "react";
+import { useState } from "react";
+import "./styles.css";
+
+const gitHubURL = "https://api.github.com/repos/tannerlinsley/react-query";
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const loadData = async () => {
+    try {
+      const response = await axios.get(gitHubURL);
+      setData(JSON.stringify(response.data));
+    } catch (ex) {
+      setError("There was an error!");
+      console.error(ex);
+    }
+    setIsLoading(false);
+  };
+
+  if (isLoading) return <div className="App">Loading...</div>;
+  if (error) return <div className="App">{error}</div>;
+
+  return (
+    <div className="App">
+      <button onClick={loadData}>Click me to load data!</button>
+      {<div>{data}</div>}
+    </div>
+  );
+}
